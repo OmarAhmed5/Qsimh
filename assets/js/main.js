d389af57f52f06839c3287df4abc1801bfec4f3d
Omar Ahmed ElSayed <64767096+OmarAhmed5@users.noreply.github.com>
2021-05-28 16:54:37 +0200

    Edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,21 +303,8 @@ $('#category').on('change', function (e) {
 
 // ! Image Preview
 
+// Add - Edit Qsimh Page
 $(document).ready(function () {
-    $(document).on('change', '.btn-file :file', function () {
-        var input = $(this),
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [label]);
-    });
-    $('.btn-file :file').on('fileselect', function (event, label) {
-        var input = $(this).parents('.input-group').find(':text'),
-            log = label;
-        if (input.length) {
-    input.val(log);
-        } else {
-            if (log) alert(log);
-        }
-    });
     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -331,4 +318,19 @@ $(document).ready(function () {
     readURL(this);
     });
 });
-  
+
+// Famous Page
+$(document).ready(function () {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+    $('#famous-img-upload').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#famousImgInp").change(function () {
+    readURL(this);
+    });
+});
